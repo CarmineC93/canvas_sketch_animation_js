fe656d793447f63f7e7e0f43a018a144ea7d778f
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-09 15:52:10 +0100

    refactoring classes code

--- animation.js
+++ animation.js
@@ -8,22 +8,14 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-    
-//2 creo due istanze di oggetti dalla classe Point
-const pointA = new Point(800, 400, 10);
-const pointB = new Point(300, 700, 10);
 
-//3 creo la prima figura con le proprietà dell'oggetto A
-context.beginPath();
-context.arc(pointA.x, pointA.y, pointA.radius, 0, Math.PI * 2);
-context.fillStyle = 'black';
-context.fill();
+//5 creo due istanze di oggetti dalla classe Agent
+const agentA = new Agent(800, 400);
+const agentB = new Agent(300, 700);
 
-//  e la seconda con le proprietà del Point B
-context.beginPath();
-context.arc(pointB.x, pointB.y, pointB.radius, 0, Math.PI * 2);
-context.fill();
-
+//6 ora posso generare le figure
+agentA.draw(context);
+agentB.draw(context);
 
   };
 };
@@ -32,11 +24,26 @@ context.fill();
 
 canvasSketch(sketch, settings);
 
-//1 creo una classe Point, per creare oggetti sferici, con coordinate e raggio come proprietà
+//1 creo una classe Point, per fissare la posizione degli oggetti sferici, con coordinate come proprietà
 class Point {
-  constructor (x, y, radius){
+  constructor (x, y){
     this.x = x;
     this.y = y;
-    this.radius = radius;
   }
+}
+
+//4 creo una nuova classe con all'interno il punto di prima(posizione) che sarà la vera sfera
+class Agent {
+  constructor (x, y){
+    this.pos = new Point(x, y);
+    this.radius = 10; //il raggio sarà uguale per tutte le figure
+  }
+  //do un metodo alla classe per generare le figure sferiche
+  draw(context){
+    context.beginPath();
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+
+  }
+
 }
